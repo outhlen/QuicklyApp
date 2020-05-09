512ed087626d3fa381031cfd6d630e94f79184d6
e <2595241187@qq.com>
2020-05-09 18:00:57 +0800

    地图so文件修改

---
+++ app/src/main/java/com/escort/carriage/android/entity/response/ResponseOrderBeanEntity.java
@@ -0,0 +1,7 @@
+package com.escort.carriage.android.entity.response;
+
+import com.androidybp.basics.okhttp3.entity.ResponceJsonEntity;
+import com.escort.carriage.android.entity.bean.home.OrderInfoEntity;
+
+public class ResponseOrderBeanEntity   extends ResponceJsonEntity<OrderInfoEntity> {
+}
